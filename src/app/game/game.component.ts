02e7d20c706845e82a3521c119d9f636350dece0
Devdatt sawant <sawantdevdatt@gmail.com>
2024-02-12 07:44:54 -0800

    Score element
    Added score element to maintain user winning count

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 import * as trending from '../../assets/trendingItems.json';
 import { Observable, interval, map, startWith } from 'rxjs';
@@ -14,89 +14,117 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
 export class GameComponent implements OnInit {
 
   trendingItem: any;
-  item1:any;
-  item2:any;
-  item1Url:any;
-  item2Url:any;
+  item1: any;
+  item2: any;
+  item1Url: any;
+  item2Url: any;
   random1: number = 0;
   random2: number = 0;
   link: any;
-  enableCounter:any
+  item1Counter: any;
+  item2Counter: any;
+  enableCounter: any
+  score: number = 0;
 
   finalLinks = ""
   //link[0]="https://youtu.be/UdCkfcYcLUM?si=8vJlUVQRGn3a92jJ";
 
-  constructor(private searchService: GoogleSearchService, private e : ElementRef) {
+  constructor(private searchService: GoogleSearchService, private e: ElementRef) {
     this.trendingItem = trending;
   }
   ngOnInit(): void {
-   
+
     this.generateData();
     // console.log(this.trendingItem[ranodm1],this.trendingItem[ranodm2]);
 
-    VanillaTilt.init(this.e.nativeElement.querySelectorAll(".cards"),{ max: 3, speed: 3, scale: 1.05 });
-      
+    VanillaTilt.init(this.e.nativeElement.querySelectorAll(".cards"), { max: 3, speed: 3, scale: 1.05 });
+
   }
 
-  
-getResult($event:any){
+
+  getResult(item1: any, item2: any) {
+
     this.enableCounter = true;
-}
-  generateData(){
+
+    var startCounter = 0;
+    var endCounter = Number(item1["percentage"]);
+      while (startCounter != endCounter) {
+
+
+        this.item1Counter = startCounter;
+        startCounter++
+      }
+    console.log(Number(item1["percentage"]) + " " + Number(item2["percentage"]))
+    if (Number(item1["percentage"]) >= Number(item2["percentage"])) {
+      this.score++;
+    } else {
+      this.score = 0;
+      console.log("Incorrect ans")
+    }
+    setTimeout(() => {
+      console.log("this is the first message");
+      this.enableCounter = false;
+      this.generateData()
+    }, 3000);
+    console.log("Score is " + this.score);
+
+  }
+
+  generateData() {
     do {
       this.random1 = Math.floor(Math.random() * this.trendingItem.length);
       this.random2 = Math.floor(Math.random() * this.trendingItem.length);
-    }while (this.random1 != this.random2) {
+    } while (this.random1 != this.random2) {
       this.random1 = Math.floor(Math.random() * this.trendingItem.length);
       this.random2 = Math.floor(Math.random() * this.trendingItem.length);
     }
 
     this.item1 = this.getInputData(this.random1)
     this.item2 = this.getInputData(this.random2)
-  
-  //  this.getInputImage(this.item1["name"],"Item1")
-  //  this.getInputImage(this.item2["name"],"Item2")
+
+    //  this.getInputImage(this.item1["name"],"Item1")
+    //  this.getInputImage(this.item2["name"],"Item2")
     //this.item1Url)
-   //console.log(this.trendingItem[this.random2]['url'])
+    //console.log(this.trendingItem[this.random2]['url'])
   }
 
-  getInputImage(keyword: string,item:string) {
-    
-     this.searchService.getImageBySearch(keyword).subscribe({
-      next: (result) =>{
-       var respon = result["items"][0];
-       console.log(respon);
-       this.setInputImage(respon,item);
-      
-     },
-     error:(error) =>{
-       console.log(error);
+  getInputImage(keyword: string, item: string) {
 
-     }
-   })
-  
-   
+    this.searchService.getImageBySearch(keyword).subscribe({
+      next: (result) => {
+        var respon = result["items"][0];
+        console.log(respon);
+        this.setInputImage(respon, item);
 
-}
+      },
+      error: (error) => {
+        console.log(error);
 
-setInputImage(respon:any,item:string){
+      }
+    })
 
-  var Url
-  if(respon["link"].includes("youtube")){
-    Url ="http://img.youtube.com/vi/"+respon.link.split('=')[1]+"/0.jpg"
-   }else {
-    Url = respon.pagemap.metatags[0]["og:image"]
-   }
-  console.log(Url)
-   if(item =="Item1" ){
-     this.item1Url = Url
-   }else{
-    this.item2Url = Url
-   }
-}
 
-  getInputData(item:number){
-    console.log("fetching input data for"+item);
+
+  }
+
+  setInputImage(respon: any, item: string) {
+
+    var Url
+    if (respon["link"].includes("youtube")) {
+      Url = "http://img.youtube.com/vi/" + respon.link.split('=')[1] + "/0.jpg"
+    } else {
+      Url = respon.pagemap.metatags[0]["og:image"]
+    }
+    console.log(Url)
+    if (item == "Item1") {
+      this.item1Url = Url
+    } else {
+      this.item2Url = Url
+    }
+  }
+
+  getInputData(item: number) {
+    console.log("fetching input data for" + item);
     var trendItem = this.trendingItem[item];
     console.log(trendItem);
     return trendItem;
